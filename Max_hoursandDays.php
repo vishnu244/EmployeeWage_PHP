1e5be460b49d72662a6f19f6490c619e3beec37b
vishnu244 <vishnunali244@gmail.com>
2022-08-06 09:47:46 +0530

    [Vishnu]-Calculating Employee Wage per Month including maximum hours and Days

--- Max_hoursandDays.php
+++ Max_hoursandDays.php
@@ -12,19 +12,15 @@ if ($random_number == 1)
 {
     echo "Employee is Present " ;
     echo "Employee is Full Time";
-
 }
 else if ($random_number == 2)
 {
     echo "Employee is Present ";
     echo "Employee is Part Time";
-
 }
-
 else
 {
     echo "Employee is Absent "; 
-
 }
 
 while (($empHrs < $maxworkinghrs) && ($workingdays < $maxworkingdays))
@@ -51,10 +47,4 @@ echo "Total working Days Per Month :",$workingdays;
 $monthsalary = $Wage_PerHour * $empHrs;
 echo "Total Salary of Employee per Month : ",$monthsalary;
 
-/*
-$empWage=$Wage_PerHour*$empHrs;
-echo "Wage of Employee = ",$empWage;
-
-$empwage_mnt =  $empWage * $Emp_working_days_mnt;
-echo "Wage of Employee per month = ",$empwage_mnt;*/
 ?>
